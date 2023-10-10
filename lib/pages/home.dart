f51a83338f221d13857e562c387f77b435a66037
latyr0503 <latyr0503@gmail.com>
2023-10-10 15:05:02 +0000

    ajout page home

---
+++ lib/pages/home.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: const Color.fromARGB(248, 248, 248, 248),
+      child: SingleChildScrollView(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 75,
+              ),
+              Container(
+                height: 300,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/Welcome 1.png"))),
+              ),
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                  color: Colors.white,
+                ),
+                padding: const EdgeInsets.all(20.0),
+                child: const Column(
+                  children: [
+                    Text(
+                      "Votre application ultime de prise de rendez-vous chez le médecin",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10.0)),
+                    Text(
+                      "Prenez rendez-vous sans effort et gérez votre parcours de santé.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey,
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    CupertinoButton(
+                      onPressed: () {
+                        print("Bienvenue cher utilisateur");
+                      },
+                      color: Colors.blue[800],
+                      borderRadius: BorderRadius.circular(50),
+                      child: const Text("Let's Get Started"),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Vous avez déjà un compte ?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text('Se connecter'),
+                        ),
+                        const Padding(padding: EdgeInsets.only(bottom: 60.0)),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ]),
+      ),
+    ));
+  }
+}
