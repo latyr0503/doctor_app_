9c338348673ec7b09b3e7ba9f004401ea0b3caf8
LAMINE-CISSE-Dev <lc327300@gmail.com>
2023-10-11 20:46:11 +0000

    Depot des trois pages

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,18 +1,46 @@
+import 'package:doctor_app/pages/accesslocation.dart';
 import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
 class Welcome_page extends StatelessWidget {
-  const Welcome_page({super.key});
+  const Welcome_page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Bienvenue dans la page d'acceuil", style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.w900,
-          color: Color.fromRGBO(21, 101, 192, 1)
-        ),),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Bienvenue dans la page d'accueil",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w900,
+                color: Color.fromRGBO(21, 101, 192, 1),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => onDone(context),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: const Text('Voir la page Location'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDone(BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const AccessLocation(),
       ),
     );
   }
