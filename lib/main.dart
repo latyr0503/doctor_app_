f99d1ccaa68a90c800e91c5a2179b9ba85d66d79
LAMINE-CISSE-Dev <lc327300@gmail.com>
2023-10-12 09:43:39 +0000

    latyr

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:doctor_app/pages/home.dart';
-import 'package:doctor_app/pages/profile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,8 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade700),
         useMaterial3: true,
       ),
-      // home: IntroScreen(),
-      home: const Profile(),
+      home: const HomePage(),
     );
   }
 }
