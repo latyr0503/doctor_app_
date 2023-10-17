ee543037891529ad210597eebbec3aa0aec03360
LAMINE-CISSE-Dev <lc327300@gmail.com>
2023-10-17 11:46:56 +0000

    retification des bouton dans la page rendez vous

--- lib/pages/appoitment.dart
+++ lib/pages/appoitment.dart
@@ -5,14 +5,14 @@ import 'package:flutter/material.dart';
 class Appoitment extends StatelessWidget {
   Appoitment({super.key});
   final categories = [
-    {'day': "Today", 'jour': "4 Oct", 'heure': "7:00 PM"},
-    {'day': "Mon", 'jour': "5 Oct", 'heure': "7:30 PM"},
-    {'day': "Tue", 'jour': "6 Oct", 'heure': "8:00 PM"},
-    {'day': "Wed", 'jour': "7 Oct",'heure': "8:30 PM"},
-    {'day': "Thi", 'jour': "8 Oct",'heure': "9:00 PM"},
-    {'day': "Fri", 'jour': "9 Oct", 'heure': "9:30 PM"},
-    {'day': "Sun", 'jour': "10 Oct", 'heure': "10:00 PM"},
-    {'day': "Sat", 'jour': "11 Oct", 'heure': "10:30 PM"},
+    {'day': "Today\n4 Oct", 'heure': "7:00 PM"},
+    {'day': "Mon\n5 Oct", 'heure': "7:30 PM"},
+    {'day': "Tue\n6 Oct", 'heure': "8:00 PM"},
+    {'day': "Wed\n7 Oct", 'heure': "8:30 PM"},
+    {'day': "Thi\n8 Oct", 'heure': "9:00 PM"},
+    {'day': "Fri\n9 Oct", 'heure': "9:30 PM"},
+    {'day': "Sun\n10 Oct", 'heure': "10:00 PM"},
+    {'day': "Sat\n11 Oct", 'heure': "10:30 PM"},
   ];
   @override
   Widget build(BuildContext context) {
@@ -63,7 +63,7 @@ class Appoitment extends StatelessWidget {
                 ),
               ),
             ),
-             Container(
+            Container(
               margin: const EdgeInsets.only(top: 20, left: 33),
               child: const Text(
                 "Day",
@@ -82,135 +82,136 @@ class Appoitment extends StatelessWidget {
                 ),
                 Container(
                   height: 80,
-                  color: Colors.white,
                   child: ListView.separated(
                     scrollDirection: Axis.horizontal,
-
                     itemBuilder: (context, index) {
                       return SingleChildScrollView(
                         child: Container(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.black, // Couleur de la bordure
-                              width: 1.0, // Largeur de la bordure
-                            ),
-                            borderRadius: BorderRadius.circular(
-                                30.0), // Rayon des coins arrondis
-                          ),
                           child: Column(
                             children: <Widget>[
-                              Text(categories[index]['day'] as String),
-                              Text(categories[index]['jour'] as String, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+                              ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 40, vertical: 25),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                              ),
+                                child: Text(categories[index]['day'] as String,  textAlign: TextAlign.center, style: TextStyle(color: Colors.black , fontSize: 15)),
+                              ),
                             ],
                           ),
                         ),
-                       
                       );
                     },
                     separatorBuilder: (context, index) => const SizedBox(
                       width: 33,
                     ),
                     itemCount: categories.length,
-                    
                   ),
                 ),
-                Container(
-              margin: const EdgeInsets.only(right: 380, bottom: 10),
-              child: const Text(
-                "Time",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-               Container(
-             
-                  height: 80,
-                  color: Colors.white,
-                  child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-
-                    itemBuilder: (context, index) {
-                      return SingleChildScrollView(
-                        child: Container(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.black, // Couleur de la bordure
-                              width: 1.0, // Largeur de la bordure
-                            ),
-                            borderRadius: BorderRadius.circular(
-                                30.0), // Rayon des coins arrondis
-                          ),
-                          child: Column(
-                            children: <Widget>[
-                              Text(categories[index]['heure'] as String, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-                            ],
-                          ),
-                        ),
-                       
-                      );
-                    },
-                    separatorBuilder: (context, index) => const SizedBox(
-                      width: 33,
-                    ),
-                    itemCount: categories.length,
-                    
-                  ),
-                ),
-
-                
               ]),
             ),
-             Padding(
+            
+             Container(
+                  margin: const EdgeInsets.only(top:10, left: 30,),
+                  child: const Text(
+                    "Time",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+
+            Container(
+              padding: EdgeInsets.only(top: 10, left: 30, right: 30),
+              child: Column(children: <Widget>[
+                const SizedBox(
+                  height: 5,
+                ),
+                
+               
+                Container(
+                  height: 80,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return SingleChildScrollView(
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              ElevatedButton(
+                                onPressed: () {},
+                                 style: ElevatedButton.styleFrom(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 50, vertical: 25),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                                child: Text(categories[index]['heure'] as String,
+                                style: TextStyle(color: Colors.black,
+                                fontSize: 20),),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) => const SizedBox(
+                      width: 33,
+                    ),
+                    itemCount: categories.length,
+                  ),
+                ),
+              ]),
+            ),
+
+            
+            Padding(
               padding: const EdgeInsets.only(left: 30.0, right: 30.0),
               child: TextField(
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: const BorderSide(width: 0.8),
-                ),
-                hintText: 'Want a custom Schedule ?',
-                suffixIcon: TextButton(
-                  onPressed: (){},
-                  child: const Text('Request Schedule'),
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 15.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: const BorderSide(width: 0.8),
+                  ),
+                  hintText: 'Want a custom Schedule ?',
+                  suffixIcon: TextButton(
+                    onPressed: () {},
+                    child: const Text('Request Schedule'),
+                  ),
                 ),
               ),
-),
-
             ),
 
             Container(
               padding: EdgeInsets.only(top: 10, left: 30, right: 30),
-
-               child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue[800],
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 120, vertical: 20),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[800],
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 120, vertical: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: const Text(
+                  "Make Appointment",
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              child: const Text(
-               "Make Appointment",
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            
             ),
             SizedBox(height: 50)
           ],
-          
         ));
   }
 }
--- lib/pages/selectpackage.dart
+++ lib/pages/selectpackage.dart
@@ -5,33 +5,48 @@ class SelectPackage extends StatelessWidget {
   final categories = [
    {
     'icon': Icons.message,
-    'color': const Color.fromARGB(239, 210, 68, 8),
-    'title': 'Arcade',
+    'color': const Color.fromRGBO(187, 222, 251, 1),
+    'title': 'Messaging',
    },
       {
     'icon': Icons.call,
-    'color': const Color.fromARGB(255, 201, 12, 12),
-    'title': 'Tab Title',
+    'color': const Color.fromRGBO(187, 222, 251, 1),
+    'title': 'Voice Call',
    },
       {
     'icon': Icons.video_call,
-    'color': const Color.fromARGB(255, 13, 199, 16),
-    'title': 'Color Title',
+    'color': const Color.fromRGBO(187, 222, 251, 1),
+    'title': 'Video Call',
     
    },
       {
     'icon': Icons.person,
-    'color': const Color.fromARGB(255, 16, 88, 224),
-    'title': 'My Home',
+    'color': const Color.fromRGBO(187, 222, 251, 1),
+    'title': 'In Person',
    },
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+        appBar: AppBar(
+            leading: Builder(
+                builder: (context) => Container(
+                      margin: const EdgeInsets.only(left: 20),
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        onPressed: () {},
+                      ),
+                    )),
+            title: const Center(
+              child: Text(
+                'Select Package',
+              ),
+            )),
+    
+   body: Container(
       height: 500,
       decoration: const BoxDecoration(
-        color: Color(0xFFF6F0FF),
         borderRadius: BorderRadius.only(
           topLeft:  Radius.circular(20),
           topRight:  Radius.circular(20),
@@ -41,7 +56,7 @@ class SelectPackage extends StatelessWidget {
         children: [
           Container(
               height: 140,
-              color: Colors.blue,
+             
               child: ListView.separated(
                 scrollDirection: Axis.horizontal,
                 itemBuilder: ((context, index) =>  Column(
@@ -52,7 +67,7 @@ class SelectPackage extends StatelessWidget {
                         borderRadius: BorderRadius.circular(20),
                         color: (categories[index]['color'] as Color)
                       ),
-                      child: Icon(categories[index]['icon'] as IconData)
+                      child: Icon(categories[index]['icon'] as IconData, color: Colors.blue,)
                    ),
 //                   Container(
 //                     child: Image(image: categories[index]['images'],
@@ -66,7 +81,8 @@ class SelectPackage extends StatelessWidget {
           ),
         ],
       ),
-      );
+      )
+       );
 
   }
 }
